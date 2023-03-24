88c80066e7c41f72285747db396c1ef066f5a5b4
1slander <hectorgomendez@gmail.com>
2023-03-24 15:15:12 +0000

    iteration 3 done

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length=0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    return this.items.sort((a,b)=>a-b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos<=this.length){
+      return this.items.at(pos);
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
   max() {}
 
